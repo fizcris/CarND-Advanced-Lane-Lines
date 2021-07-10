707729d08459d4661147705bc8bb6fa3b810f55d
fizcris <fizcris@gmail.com>
2021-07-10 10:54:13 +0200

    feat: added average smooting to coefs + bugfixes

--- Line.py
+++ Line.py
@@ -7,7 +7,7 @@ class Line():
         self.missdetections = 0  
         
         # x values of the last n fits of the line
-        self.recent_xfitted = np.array([])
+        self.recent_xfitted = np.array([0])
         #average x values of the curernt iteration
         self.currentx = 0  
         #average x values of the fitted line over the last n iterations
@@ -20,14 +20,14 @@ class Line():
         #difference in fit coefficients between last and new fits
         self.poly_diffs = np.array([])
         #All difference in fit coefficients between last and new fits
-        self.all_poly_diffs = np.array([])
+        self.all_poly_diffs = np.array([[0,0,0]]) 
         #Last fitted coefs
-        self.recent_poly_fits = np.array([])
+        self.recent_poly_fits = np.array([[0,0,0]]) 
         
         #Evaluated polynomial over line X points
         self.poly_plotx = np.array([])  
         #Evaluated polynomial over line Y points
-        self.poly_ploty = np.array([]) 
+        self.poly_ploty = np.linspace(0, 720-1, 720 )
 
         #x values for detected line pixels
         self.allx = None  
@@ -52,7 +52,7 @@ class Line():
         othewise discarded
         """
          # Not First iteration
-        if self.recent_xfitted.size != 0:
+        if np.any((self.recent_xfitted != 0)):
             # Check for outliers
             if (abs(currentx - self.bestx) < limit):
                 self.currentx = currentx
@@ -68,6 +68,7 @@ class Line():
             # First iteration
             self.currentx = currentx
             self.bestx = currentx
+            self.recent_xfitted = currentx
             
         self.recent_xfitted = np.append(self.recent_xfitted, self.bestx)  
     
@@ -94,42 +95,46 @@ class Line():
         self.radius_of_curvature = ((1 + (2*self.poly_best_fit[0]*y_eval*ym_per_pix + self.poly_best_fit[1])**2)**1.5) / np.absolute(2*self.poly_best_fit[0])
 
 
-    
-               
-    def updateCoeffsLine(self,detected, current_fit, left_fitx, ploty, coefLimits=[1,1,10] ):
+    def updateCoeffsLine(self,detected, current_fit, left_fitx, ploty, coefLimits=[1,1,10], movingAvg=5 ):
         """Updates the line ploynom equation coeficients
         for the current removing outliers and applying moving average filters
         to coeffs
         """
-        
         # Not First iteration
-        if self.recent_poly_fits.size != 0:
+        if  np.any((self.recent_poly_fits != 0)):
             if detected:
                 if any(current_fit): 
                     self.poly_diffs = np.subtract(self.poly_best_fit,current_fit)
-                    self.all_poly_diffs = np.append(self.all_poly_diffs,self.poly_diffs)
+                    self.all_poly_diffs = np.vstack((self.all_poly_diffs,self.poly_diffs)) 
                 # If outlier
                 if (abs(self.poly_diffs[0]) > coefLimits[0] or abs(self.poly_diffs[1]) > coefLimits[1] or abs(self.poly_diffs[2]) > coefLimits[2] ):
+                    print("missdetection")
+                    print(self.poly_diffs)
                     self.detected = False
                     self.missdetections += 1 
                 
-                else:# If not outlier
+                else:# If not outlier (Good detection)
                     self.detected = True
                     self.missdetections = 0 
-                    # TODO low pass filter coefs                    
-                    self.poly_best_fit = current_fit
-                    self.recent_poly_fits = np.append(self.recent_poly_fits, self.poly_best_fit)
+                    # Mean average filter coefs                    
+                    x = np.vstack((self.recent_poly_fits,current_fit)) 
+                    c0 = moving_average(x[:,0], movingAvg)[-1]
+                    c1 = moving_average(x[:,1], movingAvg)[-1]
+                    c2 = moving_average(x[:,2], movingAvg)[-1]
+                    self.poly_best_fit = np.array([c0,c1,c2]) 
+                    self.recent_poly_fits = np.vstack((self.recent_poly_fits,self.poly_best_fit)) 
+                    #print(self.recent_poly_fits)
+                    self.poly_ploty = ploty                  
+                    self.poly_plotx = np.polyval(self.poly_best_fit, ploty)
                     
-                    self.poly_plotx = left_fitx
-                    self.poly_ploty = ploty
             else: #Not detected
                 pass
         
         # First iteration
         else:
             self.poly_best_fit = current_fit
-            self.recent_poly_fits = np.append(self.recent_poly_fits, self.poly_best_fit) 
-            
+            self.recent_poly_fits = np.array([current_fit])
+
             self.poly_plotx = left_fitx
             self.poly_ploty = ploty
         
